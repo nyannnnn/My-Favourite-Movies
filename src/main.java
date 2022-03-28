41b99c890765f2f92db291bd770328f4f68e9605
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2022-03-28 14:26:09 -0400

    i did it!!!!

--- src/main.java
+++ src/main.java
@@ -1,6 +1,31 @@
+import java.io.*;
+import java.util.*;
 
 public class main {
 
-	
+	public static void main(String args []) {
+		ArrayList <movies> movie = new ArrayList<>();
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("file.txt"));
+			String line = "";
+			while((line = in.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line.trim());
+				String rating = st.nextToken();
+				String name = "";
+				int size = st.countTokens()-1;
+				for(int i = 0; i < size; i++) {
+					name += st.nextToken() + " ";
+				}
+				name = name.trim();
+				String genre = st.nextToken();
+				movie.add(new movies(rating, name, genre));
+			}
+			
+		}catch(FileNotFoundException e) {
+			System.out.println("File not found");
+		}catch(IOException e) {
+			System.out.println("IOException!!!!!");
+		}
+	}
 	
 }
--- src/movies.java
+++ src/movies.java
@@ -2,9 +2,48 @@
 public class movies {
 
 	private String movie;
-	private int rating;
+	private String rating;
 	private String genre;
+
+	private static int numMovies;
+	
+	//getters and setters
+	public String getMovie() {
+		return movie;
+	}
+
+	public void setMovie(String movie) {
+		this.movie = movie;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public static int getNumMovies(int numMovies) {
+		return numMovies;
+	}
+	
+	//constructor
+	public movies(String rating, String movie, String genre) {
+		this.movie = movie;
+		this.rating = rating;
+		this.genre = genre;
+		numMovies++;
+	}
 	
 	
-	
+
 }
